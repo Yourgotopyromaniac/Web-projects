6a1be7db75e74691be4b025b7d98d1b7297d92d8
Yourgotopyromaniac <64610890+Yourgotopyromaniac@users.noreply.github.com>
2022-04-16 09:05:19 -0700

    Add files via upload

---
+++ Technical Documentation Page/script.js
@@ -0,0 +1,24 @@
+function darkMode() 
+{
+  
+  document.getElementById("body-id").classList.toggle("light-mode");
+  document.getElementById("top-nav").classList.toggle("topnav-light");
+  document.getElementById("navbar").classList.toggle("nav-class-light");
+  document.getElementById("main-doc").classList.toggle("main-doc-light");
+  document.getElementById("input-box").classList.toggle("input-box-light");
+  document.getElementById("search").classList.toggle("search-light");
+  document.getElementById("search-icon").classList.toggle("search-icon-light");
+  document.getElementById("link1").classList.toggle("link-dark");
+  document.getElementById("link2").classList.toggle("link-dark");
+  document.getElementById("link3").classList.toggle("link-dark");
+  document.getElementById("link4").classList.toggle("link-dark");
+  document.getElementById("link5").classList.toggle("link-dark");
+  document.getElementById("link6").classList.toggle("link-dark");
+  document.getElementById("link7").classList.toggle("link-dark");
+  document.getElementById("link8").classList.toggle("link-dark");
+  document.getElementById("link9").classList.toggle("link-dark");
+  document.getElementById("link10").classList.toggle("link-dark");
+  document.getElementById("link11").classList.toggle("link-dark");
+  document.getElementById("list-uo").classList.toggle("list-uo-light");
+  document.getElementById("back-to-top").classList.toggle("back-to-top-dark");
+} 
