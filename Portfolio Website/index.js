a5d2dd26513fc6ddd6a4f436e23c0ede9abd22ea
Yourgotopyromaniac <64610890+Yourgotopyromaniac@users.noreply.github.com>
2022-04-22 15:08:10 -0700

    Add files via upload

--- Portfolio Website/index.js
+++ Portfolio Website/index.js
@@ -1,7 +1,5 @@
 function darkMode() 
 {
-  //document.getElementById("navbar").classList.toggle("nav-class-light");
-  //document.getElementById("main-doc").classList.toggle("main-doc-light");
   document.getElementById("link1").classList.toggle("link-dark");
   document.getElementById("link2").classList.toggle("link-dark");
   document.getElementById("link3").classList.toggle("link-dark");
@@ -11,16 +9,30 @@ function darkMode()
   document.getElementById("theme-sun").classList.toggle("link-dark");
   document.getElementById("theme-moon").classList.toggle("link-dark");
   document.getElementById("about").classList.toggle("about-dark");
-  document.getElementById("hobby1").classList.toggle("hobby1-dark");
-  document.getElementById("hobby2").classList.toggle("hobby2-dark");
-  document.getElementById("hobby3").classList.toggle("hobby3-dark");
-  document.getElementById("hobby4").classList.toggle("hobby4-dark");
-  document.getElementById("hobby5").classList.toggle("hobby5-dark");
-  document.getElementById("hobby6").classList.toggle("hobby6-dark");
+  document.getElementById("skill1").classList.toggle("skill1-dark");
+  document.getElementById("skill2").classList.toggle("skill2-dark");
+  document.getElementById("skill3").classList.toggle("skill3-dark");
+  document.getElementById("skill4").classList.toggle("skill4-dark");
   document.getElementById("logo").classList.toggle("logo-dark");
-  document.getElementById("projects").classList.toggle("project-dark");
-
-  //document.getElementById("back-to-top").classList.toggle("back-to-top-dark");
+  document.getElementById("projects").classList.toggle("project-box-dark");
+  document.getElementById("project1").classList.toggle("project-tile-dark");
+  document.getElementById("project2").classList.toggle("project-tile-dark");
+  document.getElementById("project3").classList.toggle("project-tile-dark");
+  document.getElementById("project4").classList.toggle("project-tile-dark");
+  document.getElementById("project5").classList.toggle("project-tile-dark");
+  document.getElementById("proj-link1").classList.toggle("proj-link-dark");
+  document.getElementById("proj-link2").classList.toggle("proj-link-dark");
+  document.getElementById("proj-link3").classList.toggle("proj-link-dark");
+  document.getElementById("proj-link4").classList.toggle("proj-link-dark");
+  document.getElementById("proj-link5").classList.toggle("proj-link-dark");
+  document.getElementById("proj-link6").classList.toggle("proj-link-dark");
+  document.getElementById("proj-link7").classList.toggle("proj-link-dark");
+  document.getElementById("proj-link8").classList.toggle("proj-link-dark");
+  document.getElementById("proj-link9").classList.toggle("proj-link-dark");
+  document.getElementById("proj-link10").classList.toggle("proj-link-dark");
+  document.getElementById("up-to").classList.toggle("upto-dark");
+  document.getElementById("contact").classList.toggle("contact-dark");
+  document.getElementById("back-to-top").classList.toggle("back-to-top-dark");
 } 
 
 var nickName = document.getElementById("name");
@@ -44,3 +56,12 @@ function nameChange()
     
 }
 
+var cip = $(".video").hover( hoverVideo, hideVideo );
+
+function hoverVideo(e) {  
+    $('video', this).get(0).play(); 
+}
+
+function hideVideo(e) {
+    $('video', this).get(0).pause(); 
+}
