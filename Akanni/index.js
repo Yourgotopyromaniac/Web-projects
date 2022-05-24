664f1605853a2e402d72c14839a6bbdf46d47529
Yourgotopyromaniac <64610890+Yourgotopyromaniac@users.noreply.github.com>
2022-05-24 05:46:30 -0700

    Add files via upload

---
+++ Akanni/index.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("right-slider").style.width = "500px";
+  }
+  
+  function closeNav() {
+    document.getElementById("right-slider").style.width = "0";
+  }
